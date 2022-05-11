f00d4127fef648463b4cdf6c60d95f7195bfc99a
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-05-11 02:32:39 -0500

    re-wrote token mapping

--- src/tokenizer/index.ts
+++ src/tokenizer/index.ts
@@ -1,4 +1,4 @@
-const tokenizer = /(?<words>[^\s\.,!]+)|(?<spaces>[\s]+)|(?<punc>[!\.,-])/gim;
+const tokenizer = /(["'{\[].*?["'}\]])|(\s)+|([^\s\.]+)|([\.,])+/g
 
 enum TokenTypeOptions {
     InBraces = 1 << 0,
@@ -19,74 +19,88 @@ const getFilePattern = () => {
     return /^([a-zA-Z]):[\\\/]((?:[^<>:"\\\/\|\?\*]+[\\\/])*)([^<>:"\\\/\|\?\*]+)\.([^<>:"\\\/\|\?\*\s]+)$/gim;
 }
 
-const TOKENTYPELIST = [
-    {
-        type: TokenTypeOptions.InDoubleQuotes,
-        pattern: /\"[^"]*\"/gim
-    },
-    {
-        type: TokenTypeOptions.IsWhitespace,
-        pattern: /[\s]+/gim,
-    },
-    {
-        type: TokenTypeOptions.InBrackets,
-        pattern: /\[[^\]]*\]/gim
-    },
-    {
-        type: TokenTypeOptions.InParenthesis,
-        pattern: /\([^\)]*\)/gim
-    },
-    {
-        type: TokenTypeOptions.InBraces,
-        pattern: /\{[^\}]*\}/gim
-    },
-    {
-        type: TokenTypeOptions.IsFilePath,
-        pattern: getFilePattern()
-    },
-    {
-        type: TokenTypeOptions.IsNumeric,
-        pattern: /^\d+$(\.\d+)?/gim
-    },
-    {
-        type: TokenTypeOptions.IsPunctuation,
-        pattern: /[!,\.]+/gim
-    },
-    {
-        type: TokenTypeOptions.IsDate,
-        pattern: /([\d]+)([\-\./])([\d]+)([\-\./])([\d]+)|((Jan(|uary)|Feb(|ruary)|Mar(|ch)|Apr(|il)|May|Jun(|e)|Jul(|y)|Aug(|ust)|Sept(|ember)|Oct(|ober)|(Nov|Dec)(|ember))([\s\-])(|([\d]+){1,2}([\s\-]|\, ))([\d]+){4})/gim
-    }
+enum TokenFlags {
+    IsWord = 1 << 0,
+    IsPhrase = 1 << 1,
+    IsWhitespace = 1 << 2,
+    IsQuoted = 1 << 3,
+    IsSingleQuoted = 1 << 4,
+    IsDoubleQuoted = 1 << 5,
+    IsBraced = 1 << 6,
+    IsBracketed = 1 << 7,
+    IsPunctuation = 1 << 15,
+
+    // What the devil be ye?
+    IsUnknown = 1 << 16
+}
+
+const TokenFlagMapDefs = [
+    { type: TokenFlags.IsWord, pattern: /^[{\["']?[\w']+[}\]"']?$/ },
+    { type: TokenFlags.IsPhrase, pattern: /\b(\s)+\b/ },
+    { type: TokenFlags.IsWhitespace, pattern: /^[\s]+$/ },
+    { type: TokenFlags.IsQuoted, pattern: /^['"](.)+['"]$/ },
+    { type: TokenFlags.IsSingleQuoted, pattern: /^['](.)+[']$/ },
+    { type: TokenFlags.IsDoubleQuoted, pattern: /^["](.)+["]$/ },
+    { type: TokenFlags.IsBraced, pattern: /^[\[](.)+[\]]$/ },
+    { type: TokenFlags.IsBracketed, pattern: /^[{](.)+[}]$/ },
+    { type: TokenFlags.IsPunctuation, pattern: /^[.,]$/ }
 ]
-
-const findTokenType = (token: string) => {
-
-    let match: TokenTypeOptions; //TokenTypeOptions.IsUnknown;
-
-    TOKENTYPELIST.forEach((def) => {
-        if(def.pattern.test(token)) match = match | def.type
-    })
-
-    return (match) ? match : TokenTypeOptions.IsUnknown;
+type TokenFlagMap = {
+    flags: TokenFlags,
+    flagNames: string[]
 }
-
-export type Parsed = {
-    name: string;
-    type: TokenTypeOptions,
-    token: any;
-}
-
-const parse = (text: string): Parsed[] => {
-    const ast = [];
-    text.match(tokenizer).forEach((token) => {
-        const tokenType = findTokenType(token);
-        const name = TokenTypeOptions[tokenType];
-        ast.push({
-            type: tokenType,
-            name: (name), //? name : 'combined',
-            token: token
-        });
+const mapFlagsToTokens = (token): TokenFlagMap => {
+    const defs = TokenFlagMapDefs.filter((def) => {
+        const result = def.pattern.test(token);
+        return result;
     });
-    return ast;
+    
+    if (defs.length == 0) {
+        return { flags: TokenFlags.IsUnknown, flagNames: [TokenFlags[TokenFlags.IsUnknown]] };
+    } else {
+        let defType: TokenFlags;
+        let defTypeNames: string[] = [];
+
+        defs.forEach((def) => {
+            defType = defType | def.type;
+            const t = TokenFlags[def.type]
+            defTypeNames.push(t)
+        });
+
+        return { flags: defType, flagNames: defTypeNames };
+    }
+}
+
+export type Token = {
+    token: any;
+    index: number;
+    length: number;
+    flags: TokenFlags;
+    flagNames: string[];    
+}
+
+export type TokenMap = {
+    tokens: Token[];
+}
+
+const parse = (text: string): TokenMap => {
+    const tokens: Token[] = [];
+    const matches = text.matchAll(tokenizer);
+
+    for(const match of matches) {
+        const token = match[0];
+        const index = match.index;
+        const { flags, flagNames } = mapFlagsToTokens(token);
+        tokens.push({
+            token: token,
+            index: index,
+            length: token.length,
+            flags: flags,
+            flagNames: flagNames
+        });
+    }
+    
+    return { tokens: tokens };
 }
 
 export { TokenTypeOptions }
