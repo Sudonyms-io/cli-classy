c24ea9338114d0551fdb481f1f812b281c30364c
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-05-11 16:34:39 -0500

    New tokenizer.

--- src/tokenizer/index.ts
+++ src/tokenizer/index.ts
@@ -1,20 +1,5 @@
 const tokenizer = /(["'{\[].*?["'}\]])|(\s)+|([^\s\.]+)|([\.,])+/g
 
-enum TokenTypeOptions {
-    InBraces = 1 << 0,
-    InBrackets = 1 << 1,
-    InDoubleQuotes = 1 << 2,
-    InParenthesis = 1 << 3,
-    InQuotes = 1 << 4,
-    IsDate = 1 << 5,
-    IsNumeric = 1 << 6,
-    IsPunctuation = 1 << 7,
-    IsEndPuncuation = 1 << 8,
-    IsWhitespace = 1 << 9,
-    IsFilePath = 1 << 10,
-    IsUnknown = 1 << 16
-}
-
 const getFilePattern = () => {
     return /^([a-zA-Z]):[\\\/]((?:[^<>:"\\\/\|\?\*]+[\\\/])*)([^<>:"\\\/\|\?\*]+)\.([^<>:"\\\/\|\?\*\s]+)$/gim;
 }
@@ -22,48 +7,69 @@ const getFilePattern = () => {
 enum TokenFlags {
     IsWord = 1 << 0,
     IsPhrase = 1 << 1,
-    IsWhitespace = 1 << 2,
-    IsQuoted = 1 << 3,
-    IsSingleQuoted = 1 << 4,
-    IsDoubleQuoted = 1 << 5,
-    IsBraced = 1 << 6,
-    IsBracketed = 1 << 7,
+    IsWhitespace = 1 << 2,    
+    IsSingleQuoted = 1 << 3,
+    IsDoubleQuoted = 1 << 4,
+    IsBraced = 1 << 5,
+    IsBracketed = 1 << 6,
     IsPunctuation = 1 << 15,
+    // Extended
+    IsQuoted = IsDoubleQuoted | IsSingleQuoted,
+    SingleQuotedPhrase = IsPhrase | IsSingleQuoted,
+    DoubleQuotedPhrase = IsPhrase | IsDoubleQuoted,
+    QuotedPhrase = IsPhrase | IsQuoted,
+    SingleQuotedWord = IsWord | IsSingleQuoted,
+    DoubleQuotedWord = IsWord | IsDoubleQuoted,
+    QuotedWord = IsWord | IsQuoted,
 
     // What the devil be ye?
     IsUnknown = 1 << 16
 }
 
-const TokenFlagMapDefs = [
-    { type: TokenFlags.IsWord, pattern: /^[{\["']?[\w']+[}\]"']?$/ },
-    { type: TokenFlags.IsPhrase, pattern: /\b(\s)+\b/ },
-    { type: TokenFlags.IsWhitespace, pattern: /^[\s]+$/ },
-    { type: TokenFlags.IsQuoted, pattern: /^['"](.)+['"]$/ },
-    { type: TokenFlags.IsSingleQuoted, pattern: /^['](.)+[']$/ },
-    { type: TokenFlags.IsDoubleQuoted, pattern: /^["](.)+["]$/ },
-    { type: TokenFlags.IsBraced, pattern: /^[\[](.)+[\]]$/ },
-    { type: TokenFlags.IsBracketed, pattern: /^[{](.)+[}]$/ },
-    { type: TokenFlags.IsPunctuation, pattern: /^[.,]$/ }
+/**
+ * Alias for TokenFlags.
+ */
+const a_tf = TokenFlags;
+
+const TokenFlagRegExMap = [
+    { type: a_tf.IsWord, pattern: /^[{\["']?[\w']+[}\]"']?$/ },
+    { type: a_tf.IsPhrase, pattern: /\b(\s)+\b/ },
+    { type: a_tf.IsWhitespace, pattern: /^[\s]+$/ },
+    { type: a_tf.IsQuoted, pattern: /^['"](.)+['"]$/ },
+    { type: a_tf.IsSingleQuoted, pattern: /^['](.)+[']$/ },
+    { type: a_tf.IsDoubleQuoted, pattern: /^["](.)+["]$/ },
+    { type: a_tf.IsBraced, pattern: /^[\[](.)+[\]]$/ },
+    { type: a_tf.IsBracketed, pattern: /^[{](.)+[}]$/ },
+    { type: a_tf.IsPunctuation, pattern: /^[.,]$/ },
+    { type: a_tf.IsUnknown, pattern: null },
+    { type: a_tf.SingleQuotedPhrase, pattern: null },
+    { type: a_tf.DoubleQuotedPhrase, pattern: null },
+    { type: a_tf.SingleQuotedWord, pattern: null },
+    { type: a_tf.DoubleQuotedWord, pattern: null },
+    
+    { type: a_tf.QuotedPhrase, pattern: null },
+    
+    { type: a_tf.QuotedWord, pattern: null }
 ]
 type TokenFlagMap = {
     flags: TokenFlags,
     flagNames: string[]
 }
 const mapFlagsToTokens = (token): TokenFlagMap => {
-    const defs = TokenFlagMapDefs.filter((def) => {
-        const result = def.pattern.test(token);
+    const defs = TokenFlagRegExMap.filter((def) => {
+        const result = (def.pattern !== null) ? def.pattern.test(token) : [];
         return result;
     });
     
     if (defs.length == 0) {
-        return { flags: TokenFlags.IsUnknown, flagNames: [TokenFlags[TokenFlags.IsUnknown]] };
+        return { flags: a_tf.IsUnknown, flagNames: [a_tf[a_tf.IsUnknown]] };
     } else {
         let defType: TokenFlags;
         let defTypeNames: string[] = [];
 
         defs.forEach((def) => {
             defType = defType | def.type;
-            const t = TokenFlags[def.type]
+            const t = a_tf[def.type]
             defTypeNames.push(t)
         });
 
@@ -77,10 +83,6 @@ export type Token = {
     length: number;
     flags: TokenFlags;
     flagNames: string[];    
-}
-
-export type TokenMap = {
-    tokens: Token[];
 }
 
 const parse = (text: string): TokenMap => {
@@ -103,5 +105,9 @@ const parse = (text: string): TokenMap => {
     return { tokens: tokens };
 }
 
-export { TokenTypeOptions }
-export default parse;
+export type TokenMap = {
+    tokens: Token[];
+}
+
+export default parse
+export { TokenFlags, TokenFlagRegExMap }
--- test/tokenizer.spec.ts
+++ test/tokenizer.spec.ts
@@ -1,14 +1,83 @@
 import * as mocha from 'mocha';
 import { expect } from 'chai'
-import parse, { TokenTypeOptions } from '../src/tokenizer/index';
+import parse, { TokenFlagRegExMap, TokenFlags } from '../src/tokenizer/index';
+import { WORD_TOKEN_SPECs } from './tokenizer.words.helper';
+import { PHRASE_TOKEN_SPECs } from './tokenizer.phrases.helper';
+import { getEnumKeys } from './common';
 
-//import { countTokens, countTokenTypes, countTokenValues, tokensAreEqual, ftto, equals } from './tokenizer.spec.helper'
+console.log(`Tip: Set ENV variable DEBUG_MOCHA to true to enable detailed test output.`)
+
+// Have some fun...
+const emoji = {
+    fc: '🤞',  // Fingers crossed
+}
 
 describe(`Tests the tokenizer module`, function () {
-    it(`TEST`, function () {
-        const text = `"This is a quoted phrase" {bracket} [brace].           this is a word.`
-        const ast = parse(text);
-        console.log(ast);
+    let logs = [];
+    const log = (msg: any) => {
+        logs.push(`\t${msg}`)
+    }
+    beforeEach(() => {
+        
+    });
+    afterEach(() => {
+        if (process.env.DEBUG_MOCHA || process.env.MOCHA_DEBUG) {
+            while(logs.length > 0) {
+                console.log(logs.shift());
+            }
+        }
+    })
+
+    it(`Ensure each TokenFlag has a corresponding RegEx mapping.`, (done) => {
+        const flags = getEnumKeys(TokenFlags).sort();
+        
+        const regexs = TokenFlagRegExMap.map((item) => {
+            return TokenFlags[item.type];
+        }).sort();
+        
+        const msg = (flags.length > regexs.length) ? `You have more flags than regex mappings!` : `You have more regex mappings than flags!`
+
+        const flagsNotInRegExs = flags.filter((item) => {
+            return !regexs.includes(item)
+        });
+
+        const regExFlagsNotInFlags = regexs.filter((item) => {
+            return !flags.includes(item)
+        })
+
+        expect(regExFlagsNotInFlags).have.deep.equals(flagsNotInRegExs, msg);
+        
+        done();
+    })
+
+    it(`Running Single Phrase Token Tests`, function () {
+        PHRASE_TOKEN_SPECs.forEach((spec) => {
+            // Grab a description and token from the test data inputs
+            const { description, ...expected } = spec;
+
+            // Run the spec's token text thru the parser
+            const actual = parse(expected.token).tokens[0];
+
+            logs.push(`\t${emoji.fc} ${description}`);
+
+            // Parsed token should deep match spec token 
+            expect(actual, description).to.have.deep.equals(expected, description)
+        })
+    });
+
+    it.only(`Running Single Word Token Tests`, function () {
+        WORD_TOKEN_SPECs.forEach((spec) => {
+            // Grab a description and token from the test data inputs
+            const { description, ...expected } = spec;
+
+            // Run the spec's token text thru the parser
+            const actual = parse(expected.token).tokens[0];
+
+            logs.push(`\t${emoji.fc} ${description}`);
+
+            // Parsed token should deep match spec token 
+            expect(actual, description).to.have.deep.equals(expected, description)
+        })
     });
 });
 
