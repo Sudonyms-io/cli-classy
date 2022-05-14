22f3aa87c58dbfb3d8dcdf8b04a1fca53bcfbce3
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-05-13 23:36:23 -0500

    Default export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import  parse  from "./tokenizer/index";
-const text = `"This is a quoted phrase" 'me too' {bracket phrase} {bracket} [brace phrase] [brace].    'another prhase'       this is a word.`
-//const text = `"Quoted Phrase" [brace phrase]`
-const tokenMap = parse(text);
-console.log(tokenMap.tokens);
+import Stylizer from './stylizer'
+
+export default Stylizer;
